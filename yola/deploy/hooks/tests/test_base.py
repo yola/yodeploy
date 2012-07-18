2aac46d5772845b13e33b653aa663567880d075f
Stefano Rivera <stefano.rivera@yola.com>
2012-07-18 13:16:37 +0200

    Trivial tests to exercise and enforce the public DeployHook API

--- yola/deploy/hooks/tests/test_base.py
+++ yola/deploy/hooks/tests/test_base.py
@@ -1,1 +1,36 @@
+import unittest
+
 from ..base import DeployHook
+
+
+class TestDeployHook(unittest.TestCase):
+    """Trivial tests to exercise and enforce the public API"""
+
+    def setUp(self):
+        self.dh = DeployHook('test', None, '/tmp/test', '123', {}, None)
+
+    def test_hooks(self):
+        self.dh.prepare()
+        self.dh.deployed()
+
+    def test_app(self):
+        self.assertEqual(self.dh.app, 'test')
+
+    def test_root(self):
+        self.assertEqual(self.dh.root, '/tmp/test')
+
+    def test_version(self):
+        self.assertEqual(self.dh.version, '123')
+
+    def test_settings(self):
+        self.assertEqual(self.dh.settings, {})
+
+    def test_artifacts_factory(self):
+        self.assertEqual(self.dh.artifacts_factory, None)
+
+    def test_deploy_dir(self):
+        self.assertEqual(self.dh.deploy_dir, '/tmp/test/versions/123')
+
+    def test_deploy_path(self):
+        self.assertEqual(self.dh.deploy_path('foo'),
+                         '/tmp/test/versions/123/foo')
