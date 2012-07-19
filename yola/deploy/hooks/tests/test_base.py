807455aba19d413d5a821b9e7960a4a92bd04fc9
Stefano Rivera <stefano.rivera@yola.com>
2012-07-19 14:44:45 +0200

    Really, that was a bit overboard

--- yola/deploy/hooks/tests/test_base.py
+++ yola/deploy/hooks/tests/test_base.py
@@ -13,24 +13,12 @@ class TestDeployHook(unittest.TestCase):
         self.dh.prepare()
         self.dh.deployed()
 
-    def test_app(self):
+    def test_attributes(self):
         self.assertEqual(self.dh.app, 'test')
-
-    def test_root(self):
         self.assertEqual(self.dh.root, '/tmp/test')
-
-    def test_version(self):
         self.assertEqual(self.dh.version, '123')
-
-    def test_settings(self):
         self.assertEqual(self.dh.settings, {})
-
-    def test_artifacts_factory(self):
         self.assertEqual(self.dh.artifacts_factory, None)
-
-    def test_deploy_dir(self):
         self.assertEqual(self.dh.deploy_dir, '/tmp/test/versions/123')
-
-    def test_deploy_path(self):
         self.assertEqual(self.dh.deploy_path('foo'),
                          '/tmp/test/versions/123/foo')
