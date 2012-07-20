9cf424a9980573c45080a857d16c9febb86602c2
Stefano Rivera <stefano.rivera@yola.com>
2012-07-20 15:53:40 +0200

    MOAR Application test coverage

--- yola/deploy/tests/test_application.py
+++ yola/deploy/tests/test_application.py
@@ -4,6 +4,7 @@ from yola.configurator.dicts import DotDict
 
 from ..application import Application
 from ..artifacts import LocalArtifacts
+
 from . import TmpDirTestCase
 
 
@@ -41,3 +42,70 @@ class ApplicationTest(TmpDirTestCase):
         self.app.lock()
         self.assertRaises(Exception, self.app.lock)
         self.app.unlock()
+
+    def test_unpack(self):
+        self.create_tar('test.tar.gz', 'foo/bar')
+        self.app.artifacts.upload(self.tmppath('test.tar.gz'))
+        version = self.app.artifacts.versions.latest.version_id
+        os.unlink(self.tmppath('test.tar.gz'))
+
+        self.app.lock()
+        self.app.unpack(version)
+        self.app.unlock()
+
+        self.assertTMPPExists('srv', 'test', 'versions', version)
+        self.assertTMPPExists('srv', 'test', 'versions', version, 'bar')
+
+    def test_double_unpack(self):
+        self.create_tar('test.tar.gz', 'foo/bar')
+        self.app.artifacts.upload(self.tmppath('test.tar.gz'))
+        version = self.app.artifacts.versions.latest.version_id
+        os.unlink(self.tmppath('test.tar.gz'))
+
+        self.app.lock()
+        self.app.unpack(version)
+        self.app.unpack(version)
+        self.app.unlock()
+
+        self.assertTMPPExists('srv', 'test', 'versions', version)
+        self.assertTMPPExists('srv', 'test', 'versions', version, 'bar')
+
+    def test_unpack_live(self):
+        self.create_tar('test.tar.gz', 'foo/bar')
+        self.app.artifacts.upload(self.tmppath('test.tar.gz'))
+        version = self.app.artifacts.versions.latest.version_id
+        os.unlink(self.tmppath('test.tar.gz'))
+
+        os.makedirs(self.tmppath('srv', 'test', 'versions', version))
+        os.symlink(os.path.join('versions', version),
+                   self.tmppath('srv', 'test', 'live'))
+        self.assertEqual(self.app.live_version, version)
+
+        self.app.lock()
+        self.assertRaises(Exception, self.app.unpack, version)
+        self.app.unlock()
+
+    def test_swing_symlink_create(self):
+        os.makedirs(self.tmppath('srv', 'test', 'versions', 'foobar'))
+
+        self.app.lock()
+        self.app.swing_symlink('foobar')
+        self.app.unlock()
+
+        self.assertTMPPExists('srv', 'test', 'live')
+        self.assertEqual(self.app.live_version, 'foobar')
+
+    def test_swing_symlink_overwrite(self):
+        os.makedirs(self.tmppath('srv', 'test', 'versions', 'foo'))
+        os.makedirs(self.tmppath('srv', 'test', 'versions', 'bar'))
+        os.symlink(os.path.join('versions', 'foo'),
+                   self.tmppath('srv', 'test', 'live'))
+
+        self.assertEqual(self.app.live_version, 'foo')
+
+        self.app.lock()
+        self.app.swing_symlink('bar')
+        self.app.unlock()
+
+        self.assertTMPPExists('srv', 'test', 'live')
+        self.assertEqual(self.app.live_version, 'bar')
