292a65bb43b1a42f27aeb84d4310b4a7fc926b6a
Stefano Rivera <stefano.rivera@yola.com>
2012-07-23 17:48:54 +0200

    Machinary for calling hooks from a subprocess

---
+++ yola/deploy/__main__.py
@@ -0,0 +1,81 @@
+import argparse
+import imp
+import logging
+import socket
+import os
+
+import yola.deploy.artifacts
+
+
+def main():
+    p = argparse.ArgumentParser(prog='yola.deploy',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument('-c', '--config', metavar='FILE',
+                   default='/opt/deploy/config/deploy.py',
+                   help='Location of the Deploy configuration file.')
+    p.add_argument('-v', '--verbose', action='store_true',
+                   help='Increase verbosity')
+    p.add_argument('--log-fd', metavar='FD', type=int,
+                   help='File descriptor to send pickled log events to '
+                        '(not for interactive use)')
+    p.add_argument('-A', '--app', metavar='NAME',
+                   help='The application name '
+                        '(defaults to the last part of appdir)')
+    p.add_argument('appdir',
+                   help='Path to the extracted application')
+    p.add_argument('version',
+                   help='The version of the application to operate on')
+    p.add_argument('--target', metavar='TARGET',
+                   help='Target to use for artifacts')
+    p.add_argument('-H', '--hook', metavar='HOOK',
+                   help='Call HOOK in the application')
+    args = p.parse_args()
+
+    setup_logging(args.log_fd, args.verbose)
+
+    app = os.path.basename(os.path.abspath(args.appdir))
+    deploy_settings = load_settings(args.config)
+    artifacts_factory = yola.deploy.artifacts.build_artifacts_factory(
+            app, args.target, deploy_settings)
+
+    call_hook(app, args.target, args.appdir, args.version, deploy_settings,
+              artifacts_factory, args.hook)
+
+
+def setup_logging(log_fd, verbose):
+    if log_fd:
+        logging.basicConfig(level=0)
+        sock = socket.fromfd(log_fd, socket.AF_UNIX, socket.SOCK_STREAM)
+        handler = yola.deploy.ipc_logging.ExistingSocketHandler(sock)
+        logger = logging.getLogger()
+        logger.addHandler(handler)
+    elif verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig()
+
+
+def load_settings(fn):
+    '''Load deploy_settings from the specified filename'''
+    fake_mod = '_deploy_settings'
+    description = ('.py', 'r', imp.PY_SOURCE)
+    with open(fn) as f:
+        m = imp.load_module(fake_mod, f, fn, description)
+    return m.deploy_settings
+
+
+def call_hook(app, target, appdir, version, deploy_settings, artifacts_factory,
+              hook):
+    '''Load and fire a hook'''
+    fake_mod = '_deploy_hooks'
+    fn = os.path.join(appdir, 'versions', version, 'deploy', 'hooks.py')
+    description = ('.py', 'r', imp.PY_SOURCE)
+    with open(fn) as f:
+        m = imp.load_module(fake_mod, f, fn, description)
+    hooks = m.hooks(app, target, appdir, version, deploy_settings,
+                    artifacts_factory)
+    getattr(hooks, hook)()
+
+
+if __name__ == '__main__':
+    main()
---
+++ yola/deploy/tests/test__main__.py
@@ -0,0 +1,57 @@
+import os
+import subprocess
+import sys
+
+from . import TmpDirTestCase
+
+
+class TestHookery(TmpDirTestCase):
+    def test_hook(self):
+        os.makedirs(self.tmppath('test', 'versions', 'foo', 'deploy'))
+        with open(self.tmppath('test', 'versions', 'foo', 'deploy',
+                               'hooks.py'), 'w') as f:
+            f.write("""import os
+
+from yola.deploy.hooks.base import DeployHook
+
+
+class Hooks(DeployHook):
+    def prepare(self):
+        open(os.path.join(self.root, 'hello'), 'w').close()
+
+
+hooks = Hooks
+""")
+        with open(self.tmppath('config.py'), 'w') as f:
+            f.write("""import os
+
+from yola.deploy.artifacts import AttrDict
+
+
+prefix='%s'
+deploy_settings = AttrDict(
+    paths=AttrDict(
+        state=os.path.join(prefix, 'state'),
+        artifacts=os.path.join(prefix, 'artifacts'),
+    ),
+    artifacts=AttrDict(
+        provider='local',
+    ),
+)
+""" % self.tmpdir)
+        # .__main__ is needed for silly Python 2.6
+        # See http://bugs.python.org/issue2751
+        p = subprocess.Popen((
+                'python', '-m', 'yola.deploy.__main__',
+                '--config', self.tmppath('config.py'),
+                '--app', 'test',
+                '--hook', 'prepare',
+                self.tmppath('test'),
+                'foo',
+            ), env={
+                'PATH': os.environ['PATH'],
+                'PYTHONPATH': ':'.join(sys.path),
+            }, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        self.assertEqual(p.wait(), 0, 'Subprocess outputted: ' + out + err)
+        self.assertTMPPExists('test', 'hello')
