6c6ac687e5c968e663e08da1f0c572d743068bf7
Stefano Rivera <stefano.rivera@yola.com>
2012-07-24 18:13:56 +0200

    Roll build_virtualenv into yola.deploy.virtualenv

--- yola/deploy/virtualenv.py
+++ yola/deploy/virtualenv.py
@@ -1,8 +1,12 @@
+from __future__ import absolute_import
+
+import argparse
 import hashlib
 import logging
 import os
 import subprocess
 import sys
+import shutil
 import tarfile
 
 try:
@@ -19,6 +23,10 @@ except ImportError:
             return distutils.util.get_platform()
 
 import virtualenv
+
+import yola.deploy.artifacts
+import yola.deploy.config
+import yola.deploy.util
 
 log = logging.getLogger(__name__)
 # TODO: Get this from configuration
@@ -153,3 +161,85 @@ def relocateable_ve(ve_dir):
             activate.append(line)
     with open(os.path.join(ve_dir, 'bin', 'activate'), 'w') as f:
         f.write('\n'.join(activate))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+            description="Prepares virtualenv bundles. "
+                        "If a virtualenv with the right hash already exists "
+                        "in the store, it'll be downloaded and extracted, "
+                        "unless --force is specified.",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-c', '--config', metavar='FILE',
+                        default='/opt/deploy/config/deploy.py',
+                        help='Location of the Deploy configuration file.')
+    parser.add_argument('-a', '--app', metavar='NAME',
+                        help='Application name')
+    parser.add_argument('--target', metavar='TARGET',
+                        help='Target to store/retrieve virtualenvs from')
+    parser.add_argument('--env', dest='target', help=argparse.SUPPRESS)
+    parser.add_argument('--hash',
+                        action='store_true',
+                        help="Only determine the virtualenv's version")
+    parser.add_argument('-u', '--upload',
+                        action='store_true',
+                        help="Upload the resultant virtualenv to the "
+                             "repository, if there isn't one of this version "
+                             "there already")
+    parser.add_argument('-d', '--download',
+                        action='store_true',
+                        help='Download a pre-built ve from the repository')
+    parser.add_argument('-f', '--force',
+                        action='store_true',
+                        help='Overwrite the existing virtualenv')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        help='Increase verbosity')
+
+    options = parser.parse_args()
+    if not os.path.isfile('requirements.txt'):
+        parser.error('requirements.txt does not exist')
+
+    if not options.hash and (options.download or options.upload):
+        if not options.app:
+            parser.error('Application name must be specified')
+
+    if options.verbose:
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
+    else:
+        logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+
+    deploy_settings = yola.deploy.config.load_settings(options.config)
+    artifacts_factory = yola.deploy.artifacts.build_artifacts_factory(
+            options.app, options.target, deploy_settings)
+
+    version = ve_version(sha224sum('requirements.txt'))
+    if options.hash:
+        print version
+        return
+
+    existing_ve = os.path.exists('virtualenv')
+    if os.path.exists('virtualenv/.hash'):
+        with open('virtualenv/.hash', 'r') as f:
+            existing_ve = f.read().strip()
+
+    if existing_ve:
+        if existing_ve == version and not options.force:
+            options.download = False
+        else:
+            shutil.rmtree('virtualenv')
+
+    if options.download and download_ve(options.app, version,
+                                        artifacts_factory):
+        options.upload = False
+        yola.deploy.util.extract_tar('virtualenv.tar.gz', 'virtualenv')
+
+    if not os.path.isdir('virtualenv'):
+        create_ve('.')
+
+    if options.upload:
+        upload_ve(options.app, version, artifacts_factory, 'virtualenv.tar.gz',
+                  options.force)
+
+if __name__ == '__main__':
+    main()
