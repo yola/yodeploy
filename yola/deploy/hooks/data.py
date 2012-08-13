c2ed3d359e5efb2160d413a92ddd1de3f30ee530
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2012-08-13 20:31:34 +0200

    Add a hook for an app that has a persistent data dir

---
+++ yola/deploy/hooks/data.py
@@ -0,0 +1,36 @@
+import logging
+import os
+
+from yola.deploy.util import chown_r
+from .base import DeployHook
+
+log = logging.getLogger(__name__)
+
+
+class DataDirApp(DeployHook):
+    def __init__(self, *args, **kwargs):
+        super(DataDirApp, self).__init__(*args, **kwargs)
+
+    def prepare(self):
+        super(DataDirApp, self).prepare()
+        self.datadir_prepare()
+
+    def deployed(self):
+        super(DataDirApp, self).deployed()
+        self.datadir_deployed()
+
+    def django_prepare(self):
+        log.debug('Running DataDirApp prepare hook')
+        if self.config is None:
+            raise Exception("Config hasn't been loaded yet")
+
+        data_dir = os.path.join(self.root, 'data')
+        if not os.path.exists(data_dir):
+            os.mkdir(data_dir)
+        chown_r(data_dir, 'www-data', 'www-data')
+
+    def django_deployed(self):
+        data_dir = os.path.join(self.root, 'data')
+        if os.path.exists(data_dir):
+            chown_r(data_dir, 'www-data', 'www-data')
+
