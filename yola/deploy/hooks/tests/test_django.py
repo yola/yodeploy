049241caf0563c437e46dfff0c08d6c37018d1d2
Michael Joseph <michaeljoseph+github@gmail.com>
2012-08-22 14:17:59 +0200

    Test vhost attributes

--- yola/deploy/hooks/tests/test_django.py
+++ yola/deploy/hooks/tests/test_django.py
@@ -1,1 +1,13 @@
+import unittest
 from ..django import DjangoApp
+
+
+class TestDjangoHook(unittest.TestCase):
+
+    def setUp(self):
+        self.dh = DjangoApp('test', None, '/tmp/test', '123', {}, None)
+
+    def test_vhost_attributes(self):
+        self.assertEqual(self.dh.vhost_path, '/etc/apache2/sites-enabled')
+        self.assertEqual(self.dh.vhost_snippet_path, '/etc/apache2/yola.d/services')
+
