8c4354f661b818b5eabd33f4589ebd4000f15af0
Stefano Rivera <stefano.rivera@yola.com>
2012-08-28 17:56:19 +0200

    Update setup.py for 0.2.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author='Stefano Rivera',
     author_email='stefanor@yola.com',
     url='https://github.com/yola/yola.deploy',
-    version="0.2.1",
+    version="0.2.2",
     namespace_packages=['yola'],
     packages=find_packages(),
     scripts=['bin/test-templates.py'],
