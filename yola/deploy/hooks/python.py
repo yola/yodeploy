efc63d8c503da3406b2596a9353d1882a821ac40
Stefano Rivera <stefano.rivera@yola.com>
2012-11-22 16:52:25 +0200

    Don't forget to symlink if we aren't unpacking a virtualenv

--- yola/deploy/hooks/python.py
+++ yola/deploy/hooks/python.py
@@ -27,15 +27,16 @@ class PythonApp(DeployHook):
         tarball = os.path.join(ve_working, 'virtualenv.tar.gz')
         ve_unpack_root = os.path.join(ve_working, 'virtualenv')
 
-        if os.path.exists(ve_dir):
-            return
+        if not os.path.exists(ve_dir):
+            log.debug('Deploying virtualenv %s', ve_hash)
 
-        log.debug('Deploying virtualenv %s', ve_hash)
+            if not os.path.exists(ve_working):
+                os.makedirs(ve_working)
+            download_ve(self.app, ve_hash, self.artifacts_factory, tarball)
+            extract_tar(tarball, ve_unpack_root)
+            os.rename(ve_unpack_root, ve_dir)
 
-        if not os.path.exists(ve_working):
-            os.makedirs(ve_working)
-        download_ve(self.app, ve_hash, self.artifacts_factory, tarball)
-        extract_tar(tarball, ve_unpack_root)
-        os.rename(ve_unpack_root, ve_dir)
-        os.symlink(os.path.join('..', '..', 'virtualenvs', ve_hash),
-                   self.deploy_path('virtualenv'))
+        ve_symlink = self.deploy_path('virtualenv')
+        if not os.path.exists(ve_symlink):
+            os.symlink(os.path.join('..', '..', 'virtualenvs', ve_hash),
+                       ve_symlink)
