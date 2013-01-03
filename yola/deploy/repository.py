c440a86bef9119b6999bea55b7ebc6ac2db8475b
Stefano Rivera <stefano.rivera@yola.com>
2013-01-03 13:15:33 +0200

    Beginning to refactor the store from the repository

--- yola/deploy/repository.py
+++ yola/deploy/repository.py
@@ -5,6 +5,8 @@ import os
 import re
 import shutil
 
+import boto
+
 log = logging.getLogger(__name__)
 
 
@@ -35,13 +37,11 @@ class RepositoryFile(object):
         self._f.close()
 
 
-class LocalRepository(object):
-    '''Store artifacts on local machine'''
+class Repository(object):
+    '''An artifact repository'''
 
-    def __init__(self, root):
-        if not os.path.isdir(root):
-            raise Exception("root directory %s doesn't exist" % root)
-        self._root = root
+    def __init__(self, store):
+        self.store = store
 
     def get(self, app, version=None, target='master', artifact=None):
         '''
@@ -50,21 +50,82 @@ class LocalRepository(object):
         '''
         if not artifact:
             artifact = u'%s.tar.gz' % app
-        artifact_dir = os.path.join(self._root, app, target, artifact)
+        artifact_path = os.path.join(app, target, artifact)
         if not version:
-            with open(os.path.join(artifact_dir, 'latest')) as f:
+            with self.store.get(os.path.join(artifact_path, 'latest')) as f:
                 version = f.read().strip()
 
-        fn = os.path.join(artifact_dir, unicode(version))
-        meta_fn = fn + '.meta'
-        if not os.path.exists(fn) or not os.path.exists(meta_fn):
-            raise KeyError('Requested version does not exist')
-        with open(meta_fn) as f:
-            metadata = json.load(f)
-        return RepositoryFile(open(fn), metadata)
+        fn = os.path.join(artifact_path, unicode(version))
+        f, metadata = self.store.get(fn, meta=True)
+        return RepositoryFile(f, metadata)
 
-    def store(self, app, version, fp, metadata, target='master',
-              artifact=None):
+    def put(self, app, version, fp, metadata, target='master',
+            artifact=None):
+        self.store.put(app, version, fp, metadata, target, artifact)
+
+    def delete(self, app, version, target='master', artifact=None):
+        self.store.delete(app, version, target, artifact)
+
+    def list_apps(self):
+        return self.store.list_apps()
+        #return sorted(self._list(self.root))
+
+    def list_targets(self, app):
+        return self.store.list_targets(app)
+        #return sorted(self.list(os.path.join(self.root, app)))
+
+    def list_artifacts(self, app, target='master'):
+        return self.store.list_artifacts(app, target)
+        #return sorted(self.list(os.path.join(self.root, app, target)))
+
+    def list_versions(self, app, target='master', artifact=None):
+        return self.store.list_versions(app, target, artifact)
+        #if not artifact:
+        #    artifact = u'%s.tar.gz' % app
+        #directory = os.path.join(self.root, app, target, artifact)
+        #versions = []
+        #for version in self._list(directory, files=True, dirs=False):
+        #    if version == u'latest' or version.endswith(u'.meta'):
+        #        continue
+        #    versions.append(version)
+        #return sorted(versions, key=version_sort_key)
+
+    def gc(self, max_versions=10):
+        '''
+        Delete all but the most recent max_versions versions of each artifact
+        in the repository
+        '''
+        for app in self.list_apps():
+            for target in self.list_targets(app):
+                for artifact in self.list_artifacts(app, target):
+                    versions = self.list_versions(app, target, artifact)
+                    versions.reverse()
+                    for version in versions[max_versions:]:
+                        self.delete(app, version, target, artifact)
+
+
+class LocalRepositoryStore(object):
+    '''Store artifacts on local machine'''
+
+    def __init__(self, root):
+        if not os.path.isdir(root):
+            raise Exception("root directory %s doesn't exist" % root)
+        self.root = root
+
+    def get(self, path, meta=False):
+        fn = os.path.join(self.root, path)
+        meta_fn = fn + '.meta'
+        if not os.path.exists(fn) or (meta and not os.path.exists(meta_fn)):
+            raise KeyError('No such object')
+
+        if meta:
+            with open(meta_fn) as f:
+                metadata = json.load(f)
+            return open(fn), metadata
+        return open(fn)
+
+    def put(self, app, version, fp, metadata, target='master',
+            artifact=None):
         '''
         Store an artifact (fp)
         '''
@@ -72,7 +133,7 @@ class LocalRepository(object):
             artifact = u'%s.tar.gz' % app
         if version == 'latest':
             raise ValueError('Illegal version: %s' % version)
-        artifact_dir = os.path.join(self._root, app, target, artifact)
+        artifact_dir = os.path.join(self.root, app, target, artifact)
         if not os.path.isdir(artifact_dir):
             os.makedirs(artifact_dir)
 
@@ -89,7 +150,7 @@ class LocalRepository(object):
     def delete(self, app, version, target='master', artifact=None):
         if not artifact:
             artifact = u'%s.tar.gz' % app
-        artifact_dir = os.path.join(self._root, app, target, artifact)
+        artifact_dir = os.path.join(self.root, app, target, artifact)
         fn = os.path.join(artifact_dir, unicode(version))
         if not os.path.exists(fn):
             raise ValueError('No such version: %s/%s %s', app, target, version)
@@ -129,18 +190,18 @@ class LocalRepository(object):
                 raise
 
     def list_apps(self):
-        return sorted(self._list(self._root))
+        return sorted(self._list(self.root))
 
     def list_targets(self, app):
-        return sorted(self._list(os.path.join(self._root, app)))
+        return sorted(self._list(os.path.join(self.root, app)))
 
     def list_artifacts(self, app, target='master'):
-        return sorted(self._list(os.path.join(self._root, app, target)))
+        return sorted(self._list(os.path.join(self.root, app, target)))
 
     def list_versions(self, app, target='master', artifact=None):
         if not artifact:
             artifact = u'%s.tar.gz' % app
-        directory = os.path.join(self._root, app, target, artifact)
+        directory = os.path.join(self.root, app, target, artifact)
         versions = []
         for version in self._list(directory, files=True, dirs=False):
             if version == u'latest' or version.endswith(u'.meta'):
@@ -148,18 +209,13 @@ class LocalRepository(object):
             versions.append(version)
         return sorted(versions, key=version_sort_key)
 
-    def gc(self, max_versions=10):
-        '''
-        Delete all but the most recent max_versions versions of each artifact
-        in the repository
-        '''
-        for app in self.list_apps():
-            for target in self.list_targets(app):
-                for artifact in self.list_artifacts(app, target):
-                    versions = self.list_versions(app, target, artifact)
-                    versions.reverse()
-                    for version in versions[max_versions:]:
-                        self.delete(app, version, target, artifact)
+
+class S3Repository(object):
+    '''Store artifacts on S3'''
+
+    def __init__(self, bucket, access_key, secret_key):
+        s3 = boto.connect_s3(access_key, secret_key)
+        self.bucket = s3.get_bucket(bucket)
 
 
 def build_repository_factory(deploy_settings):
--- yola/deploy/tests/test_repository.py
+++ yola/deploy/tests/test_repository.py
@@ -1,6 +1,7 @@
 from StringIO import StringIO
 
-from ..repository import version_sort_key, RepositoryFile, LocalRepository
+from ..repository import (version_sort_key, RepositoryFile, Repository,
+                          LocalRepositoryStore)
 from . import unittest, TmpDirTestCase
 
 
@@ -41,55 +42,57 @@ class TestRepositoryFile(unittest.TestCase):
 class TestLocalRepository(TmpDirTestCase):
     def setUp(self):
         super(TestLocalRepository, self).setUp()
-        self.repo = LocalRepository(self.mkdir('repo'))
+        store = LocalRepositoryStore(self.mkdir('repo'))
+        self.repo = Repository(store)
 
     def assertContents(self, contents, *fragments):
         with open(self.tmppath(*fragments)) as f:
             self.assertEqual(f.read(), contents)
 
     def test_init_nonexistent(self):
-        self.assertRaises(Exception, LocalRepository, self.tmppath('missing'))
+        self.assertRaises(Exception, LocalRepositoryStore,
+                          self.tmppath('missing'))
 
-    def test_initial_store(self):
-        self.repo.store('foo', '1.0', StringIO('data'), {})
+    def test_initial_put(self):
+        self.repo.put('foo', '1.0', StringIO('data'), {})
         self.assertContents('data',
                 'repo', 'foo', 'master', 'foo.tar.gz', '1.0')
 
     def test_store_latest(self):
-        self.repo.store('foo', '1.0', StringIO('version 1'), {})
+        self.repo.put('foo', '1.0', StringIO('version 1'), {})
         self.assertContents('1.0\n',
                 'repo', 'foo', 'master', 'foo.tar.gz', 'latest')
-        self.repo.store('foo', '2.0', StringIO('version 2'), {})
+        self.repo.put('foo', '2.0', StringIO('version 2'), {})
         self.assertContents('2.0\n',
                 'repo', 'foo', 'master', 'foo.tar.gz', 'latest')
 
     def test_store_illegal_versions(self):
-        self.assertRaises(ValueError, self.repo.store, 'foo', 'latest',
+        self.assertRaises(ValueError, self.repo.put, 'foo', 'latest',
                           StringIO('version 1'), {})
 
     def test_meta(self):
-        self.repo.store('foo', '1.0', StringIO('data'), {'bar': 'baz'})
+        self.repo.put('foo', '1.0', StringIO('data'), {'bar': 'baz'})
         with self.repo.get('foo') as f:
             self.assertEqual(f.metadata, {'bar': 'baz'})
 
     def test_get(self):
-        self.repo.store('foo', '1.0', StringIO('data'), {})
+        self.repo.put('foo', '1.0', StringIO('data'), {})
         with self.repo.get('foo', '1.0') as f:
             self.assertEqual(f.read(), 'data')
 
     def test_get_latest(self):
-        self.repo.store('foo', '1.0', StringIO('old data'), {})
-        self.repo.store('foo', '2.0', StringIO('new data'), {})
+        self.repo.put('foo', '1.0', StringIO('old data'), {})
+        self.repo.put('foo', '2.0', StringIO('new data'), {})
         with self.repo.get('foo') as f:
             self.assertEqual(f.read(), 'new data')
 
     def test_get_missing(self):
-        self.repo.store('foo', '1.0', StringIO('old data'), {})
+        self.repo.put('foo', '1.0', StringIO('old data'), {})
         self.assertRaises(KeyError, self.repo.get, 'foo', '2.0')
 
     def test_targets(self):
-        self.repo.store('foo', '1.0', StringIO('master data'), {})
-        self.repo.store('foo', '2.0', StringIO('dev data'), {}, 'dev')
+        self.repo.put('foo', '1.0', StringIO('master data'), {})
+        self.repo.put('foo', '2.0', StringIO('dev data'), {}, 'dev')
         with self.repo.get('foo') as f:
             self.assertEqual(f.read(), 'master data')
         with self.repo.get('foo', target='dev') as f:
@@ -97,34 +100,34 @@ class TestLocalRepository(TmpDirTestCase):
 
     def test_list_apps(self):
         self.assertFalse(self.repo.list_apps())
-        self.repo.store('foo', '1.0', StringIO('data'), {})
+        self.repo.put('foo', '1.0', StringIO('data'), {})
         self.assertEqual(self.repo.list_apps(), ['foo'])
 
     def test_list_targets(self):
         self.assertFalse(self.repo.list_targets('foo'))
-        self.repo.store('foo', '1.0', StringIO('data'), {})
+        self.repo.put('foo', '1.0', StringIO('data'), {})
         self.assertEqual(self.repo.list_targets('foo'), ['master'])
 
     def test_list_artifacts(self):
         self.assertFalse(self.repo.list_artifacts('foo'))
-        self.repo.store('foo', '1.0', StringIO('data'), {})
+        self.repo.put('foo', '1.0', StringIO('data'), {})
         self.assertEqual(self.repo.list_artifacts('foo'), ['foo.tar.gz'])
 
     def test_list_versions(self):
         self.assertFalse(self.repo.list_versions('foo'))
-        self.repo.store('foo', '1.0', StringIO('data'), {})
+        self.repo.put('foo', '1.0', StringIO('data'), {})
         self.assertEqual(self.repo.list_versions('foo'), ['1.0'])
 
     def test_delete(self):
-        self.repo.store('foo', '1.0', StringIO('data'), {})
+        self.repo.put('foo', '1.0', StringIO('data'), {})
         self.repo.delete('foo', '1.0')
         self.assertFalse(self.repo.list_versions('foo'))
         self.assertNotTMPPExists('repo', 'foo', 'master', 'foo.tar.gz',
                                  'latest')
 
     def test_delete_older(self):
-        self.repo.store('foo', '1.0', StringIO('old data'), {})
-        self.repo.store('foo', '2.0', StringIO('new data'), {})
+        self.repo.put('foo', '1.0', StringIO('old data'), {})
+        self.repo.put('foo', '2.0', StringIO('new data'), {})
 
         self.repo.delete('foo', '1.0')
 
@@ -133,10 +136,10 @@ class TestLocalRepository(TmpDirTestCase):
             self.assertEqual(f.read(), 'new data')
 
     def test_delete_latest(self):
-        self.repo.store('foo', '1.0', StringIO('old data'), {})
+        self.repo.put('foo', '1.0', StringIO('old data'), {})
         self.assertContents('1.0\n',
                 'repo', 'foo', 'master', 'foo.tar.gz', 'latest')
-        self.repo.store('foo', '2.0', StringIO('new data'), {})
+        self.repo.put('foo', '2.0', StringIO('new data'), {})
 
         self.repo.delete('foo', '2.0')
 
@@ -149,16 +152,16 @@ class TestLocalRepository(TmpDirTestCase):
         self.assertRaises(ValueError, self.repo.delete, 'foo', '1.0')
 
     def test_delete_missing_version(self):
-        self.repo.store('foo', '1.0', StringIO('data'), {})
+        self.repo.put('foo', '1.0', StringIO('data'), {})
         self.assertRaises(ValueError, self.repo.delete, 'foo', '2.0')
 
     def test_gc(self):
-        self.repo.store('foo', '1.0', StringIO('data'), {})
-        self.repo.store('foo', '2.0', StringIO('data'), {})
-        self.repo.store('foo', '3.0', StringIO('data'), {})
-        self.repo.store('foo', '4.0', StringIO('data'), {})
-        self.repo.store('foo', '3.1', StringIO('data'), {}, 'dev')
-        self.repo.store('foo', '4.1', StringIO('data'), {}, 'dev')
+        self.repo.put('foo', '1.0', StringIO('data'), {})
+        self.repo.put('foo', '2.0', StringIO('data'), {})
+        self.repo.put('foo', '3.0', StringIO('data'), {})
+        self.repo.put('foo', '4.0', StringIO('data'), {})
+        self.repo.put('foo', '3.1', StringIO('data'), {}, 'dev')
+        self.repo.put('foo', '4.1', StringIO('data'), {}, 'dev')
         self.repo.gc(2)
         self.assertEqual(self.repo.list_versions('foo'), ['3.0', '4.0'])
         self.assertEqual(self.repo.list_versions('foo', 'dev'), ['3.1', '4.1'])
