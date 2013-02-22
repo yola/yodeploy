c1596f4bb4f93711257614ac352ccfdd34fb9d21
Stefano Rivera <stefano.rivera@yola.com>
2013-02-22 16:31:20 +0200

    build_virtualenv should be renamed to have a hyphen in the name

--- deploy/hooks.py
+++ deploy/hooks.py
@@ -11,7 +11,7 @@ class Hooks(PythonApp):
         for fn in os.listdir(self.deploy_path('yola', 'deploy', 'cmds')):
             if fn.startswith('_') or fn.endswith('.pyc'):
                 continue
-            name = fn.rsplit('.', 1)[0]
+            name = fn.rsplit('.', 1)[0].replace('_', '-')
             wrapper_name = os.path.join('/usr/local/bin', name)
             ve = self.deploy_path('virtualenv', 'bin', 'python')
             script = self.deploy_path('yola', 'deploy', 'cmds', fn)
