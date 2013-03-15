25cb52916fcc78c0a1f6c7eebeab5f25fa45ba5d
Stefano Rivera <stefano@yola.com>
2013-03-15 11:43:54 +0200

    When shell=True, the command should be a string

--- yola/deploy/cmds/build_artifact.py
+++ yola/deploy/cmds/build_artifact.py
@@ -116,11 +116,11 @@ class BuildCompat1(Builder):
         return env
 
     def upload(self):
-        subprocess.check_call(('./scripts/upload.sh',
-                               '%s-%s' % (self.app, self.dcs_target()),
-                               self.version,
-                               self.artifact,
-                              ), shell=True, env=self.build_env())
+        subprocess.check_call(' '.join('./scripts/upload.sh',
+                                       '%s-%s' % (self.app, self.dcs_target()),
+                                       self.version,
+                                       self.artifact,
+                                      ), shell=True, env=self.build_env())
 
 
 class BuildCompat2(Builder):
