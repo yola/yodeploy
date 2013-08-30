3e7171cf8c59fa0c6e2deab9838ad00391cf502b
Stefano Rivera <stefano@yola.com>
2013-08-30 17:48:39 +0200

    These attributes are in the build subsection

--- yodeploy/cmds/build_artifact.py
+++ yodeploy/cmds/build_artifact.py
@@ -157,10 +157,11 @@ class BuildCompat3(Builder):
     compat = 3
 
     def configure(self):
-        configs_dirs = [self.deploy_settings.configs_dir]
+        build_settings = self.deploy_settings.build
+        configs_dirs = [build_settings.configs_dir]
         app_conf_dir = os.path.join('deploy', 'configuration')
-        sources = config_sources(self.app, self.deploy_settings.environment,
-                                 self.deploy_settings.cluster,
+        sources = config_sources(self.app, build_settings.environment,
+                                 build_settings.cluster,
                                  configs_dirs, app_conf_dir, build=True)
         config = smush_config(sources,
                               initial={'yoconfigurator': {'app': self.app}})
