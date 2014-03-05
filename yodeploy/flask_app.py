4e58e252b2291bf083c06cf6bac73da2d4f73cc4
William Stewart <william@synthasite.com>
2014-03-05 10:56:38 +0200

    Changes flask_app to use library instead of wrapping the command calls.

--- yodeploy/flask_app.py
+++ yodeploy/flask_app.py
@@ -1,50 +1,52 @@
 import os
-import subprocess
 
 from flask import abort, Flask, jsonify, make_response, request
+from yoconfigurator.base import read_config
+
+from yodeploy.application import Application
+from yodeploy.config import find_deploy_config, load_settings
+from yodepoy.flask_auth import requires_auth
+from yodeploy.deploy import available_applications, deploy
+from yodeploy.repository import get_repository
 
 flask_app = Flask(__name__)
 
+# Set defaults
+config = find_deploy_config(False)
+deploy_settings = load_settings(config)
+repository = get_repository(deploy_settings)
 
-def get_available_apps():
-    proc = subprocess.Popen('deploy a', shell=True, stdout=subprocess.PIPE)
-    shell_output = proc.stdout.read()
-    apps = []
-    if shell_output != 'No available applications\n':
-        apps = apps.replace(' * ', '').split('\n')[1:-1]
-    return apps
-
-
-def get_app_version(app):
-    livepath = '/srv/%s/live/' % app
-    realpath = os.path.realpath(livepath)
-    version = realpath[realpath.rfind('/') + 1:]
-    return version
+QA = True if deploy_settings.build.environment == 'qa' else False
+config = read_config(os.path.join('.')) if QA else read_config(os.path.join('', 'srv', 'yodeploy', 'live'))
 
 
 @flask_app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @flask_app.route('/deploy/<app>/', methods=['GET', 'POST'])
-def deploy_app(app):
-    version = get_app_version(app)
-    if version == '' or app not in get_available_apps():
+@requires_auth
+def deploy_app(app, version=None, target='master'):
+    if app not in available_applications(deploy_settings):
         abort(404)
     if request.method == 'POST':
-        proc = subprocess.Popen('deploy deploy %s' % app, shell=True)
-        proc.wait()
-        version = get_app_version(app)
+        target = request.form.get('target')
+        version = request.form.get('version')
+        deploy(app, target, config, version, deploy_settings)
+    application = Application(app, target, repository, config)
+    version = application.live_version
     return jsonify({'application': {'name': app, 'version': version}})
 
 
 @flask_app.route('/deploy/', methods=['GET'])
+@requires_auth
 def get_all_deploy_versions():
     result = []
-    apps = get_available_apps()
+    apps = available_applications(deploy_settings)
     for app in apps:
-        version = get_app_version(app)
+        application = Application(app=app, target='master', repository=repository, settings_file=config)
+        version = application.live_version
         result.append({
             'name': app,
             'version': version
