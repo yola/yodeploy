5441368d3e6c667896e0ac88be9df9999bcf26be
Adrian Moisey <adrian@yola.com>
2014-09-25 11:15:06 +0200

    Stop chown_r choking on dangling symlinks

--- yodeploy/util.py
+++ yodeploy/util.py
@@ -16,7 +16,7 @@ def chown_r(path, user, group):
     for root, dirs, files in os.walk(path):
         os.chown(root, uid, gid)
         for fn in files:
-            os.chown(os.path.join(root, fn), uid, gid)
+            os.lchown(os.path.join(root, fn), uid, gid)
 
 
 def touch(path, user=None, group=None, perm=None):
