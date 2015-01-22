e1b91d12c39e39eff512ac4ba7f6ac60016a3d6f
William Stewart <william@synthasite.com>
2015-01-22 23:21:01 +0200

    Headers is supposed to be a dict, not list of dict

--- yodeploy/flask_auth.py
+++ yodeploy/flask_auth.py
@@ -15,7 +15,7 @@ def authenticate():
         jsonify({'error': 'Could not verify your access level for that URL.\n'
                           'You have to login with proper credentials'}),
         401,
-        [{'WWW-Authenticate': 'Basic realm="Login Required"'}])
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 
 def auth_decorator(config):
