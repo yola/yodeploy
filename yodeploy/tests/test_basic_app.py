f53860eae19c9c71b7434ce14925c6f1316e93dd
Doug Beck <doug@douglasbeck.com>
2015-01-21 20:02:53 -0500

    Add to tests a basic-configed sample app

--- yodeploy/tests/test_basic_app.py
+++ yodeploy/tests/test_basic_app.py
@@ -18,3 +18,19 @@ class TestBasicAppDeploy(unittest.TestCase):
     def test_is_deployable(self):
         deployed_file = os.path.join(self.deployed_app, 'hello-world.txt')
         self.assertTrue(os.path.exists(deployed_file))
+
+
+class TestBasicConfiguredApp(unittest.TestCase):
+
+    def setUp(self):
+        clear('basic-configed')
+        build_sample('configs')
+        build_sample('basic-configed')
+        deploy_sample('basic-configed')
+        self.deployed_app = os.path.join(
+            os.path.dirname(__file__),
+            'filesys', 'deployed', 'basic-configed', 'live')
+
+    def test_is_deployable(self):
+        deployed_config = os.path.join(self.deployed_app, 'configuration.json')
+        self.assertTrue(os.path.exists(deployed_config))
