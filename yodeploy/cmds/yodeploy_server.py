4711a97e82fbc52d7bb49b2453a21badcd6ca531
William Stewart <william@synthasite.com>
2015-01-22 16:08:49 +0200

    Remove duplicate dictionary declarations

--- yodeploy/cmds/yodeploy_server.py
+++ yodeploy/cmds/yodeploy_server.py
@@ -57,18 +57,14 @@ def get_all_deployed_versions():
     apps = available_applications(deploy_settings)
     for app in apps:
         appdir = os.path.join(deploy_settings.paths.apps, app)
+        app_result = {
+            'name': app,
+            'version': None
+        }
         if os.path.isdir(appdir):
             application = Application(app, deploy_settings_fn)
-            version = application.live_version
-            result.append({
-                'name': app,
-                'version': version
-            })
-        else:
-            result.append({
-                'name': app,
-                'version': None
-            })
+            app_result['version'] = application.live_version
+        result.append(app_result)
     return jsonify({'applications': result})
 
 
