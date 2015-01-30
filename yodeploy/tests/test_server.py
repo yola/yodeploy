6d1bb1459f518355863322b0485484e90629ede8
William Stewart <william@synthasite.com>
2015-01-30 16:26:12 +0200

    add custom test assertions for deploy server

--- yodeploy/tests/test_server.py
+++ yodeploy/tests/test_server.py
@@ -26,25 +26,50 @@ class ServerTestCase(unittest.TestCase):
 
         self.app = yodeploy_server.flask_app.test_client()
 
+    def assertResponse(self, response, status_code=200, headers=None):
+        self.assertEqual(response.status_code, status_code)
+
+        headers = headers or {}
+        for header, value in headers.iteritems():
+            self.assertEqual(response.headers[header], value)
+
+    def assertDeployedApps(self, response, status_code=200, headers=None,
+                           apps=None):
+        self.assertResponse(response, status_code, headers)
+
+        json_response = json.loads(response.data)
+
+        self.assertTrue('applications' in json_response)
+
+        apps = apps or []
+        for app in apps:
+            deployed_app_names = [
+                deployed_app['name'] for deployed_app in
+                json_response['applications']]
+            self.assertTrue(
+                any(app == app_name for app_name in deployed_app_names))
+
+    def assertDeployedApp(self, response, status_code=200, headers=None,
+                          name=None, version='1'):
+        self.assertResponse(response, status_code, headers)
+
+        json_response = json.loads(response.data)
+
+        self.assertTrue('application' in json_response)
+
     def test_no_auth_fails(self):
         response = self.app.get('/deploy/')
-        self.assertEqual(response.headers['WWW-Authenticate'],
-                         'Basic realm="Login Required"')
-        self.assertEqual(response.status_code, 401)
+        self.assertResponse(
+            response, status_code=401,
+            headers={'WWW-Authenticate': 'Basic realm="Login Required"'})
 
     def test_with_auth(self):
         response = self.app.get('/deploy/', headers=self.auth_header)
-        self.assertEqual(response.status_code, 200)
+        self.assertResponse(response)
 
     def test_get_deployed_apps(self):
         response = self.app.get('/deploy/', headers=self.auth_header)
-        json_response = json.loads(response.data)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('applications' in json_response)
-        self.assertTrue(
-            'basic-app' in app['name'] for app in json_response['applications']
-        )
+        self.assertDeployedApps(response, apps=['basic-app'])
 
     def test_get_deployed_basic_app_version(self):
         response = self.app.get('/deploy/basic-app/', headers=self.auth_header)
@@ -58,9 +83,5 @@ class ServerTestCase(unittest.TestCase):
     def test_deploy_basic_app_latest_version(self):
         response = self.app.post(
             '/deploy/basic-app/', headers=self.auth_header)
-        json_response = json.loads(response.data)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('application' in json_response)
-        self.assertEqual(json_response['application']['name'], 'basic-app')
-        self.assertEqual(json_response['application']['version'], '1')
+        self.assertDeployedApp(response, name='basic-app')
