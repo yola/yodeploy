471e078ce36e6fa79c6115dddca8995846c5b8da
Doug Beck <doug@douglasbeck.com>
2015-02-13 15:12:25 -0500

    Cleanup test output by silencing fixtures with expected failures

--- yodeploy/test_integration/samples/apache-missingconfs/deploy/hooks.py
+++ yodeploy/test_integration/samples/apache-missingconfs/deploy/hooks.py
@@ -1,10 +1,14 @@
 import mock
+import os
+import sys
 
 from os.path import join
 
 from yodeploy.hooks.apache import ApacheHostedApp
 from yodeploy.test_integration.helpers import tests_dir
 
+# cleans up test output, this hook is supposed to fail
+sys.stderr = open(os.devnull, 'w')
 
 class Hooks(ApacheHostedApp):
 
--- yodeploy/test_integration/samples/apache-missingsites/deploy/hooks.py
+++ yodeploy/test_integration/samples/apache-missingsites/deploy/hooks.py
@@ -1,10 +1,14 @@
+import os
 import mock
+import sys
 
 from os.path import join
 
 from yodeploy.hooks.apache import ApacheMultiSiteApp
 from yodeploy.test_integration.helpers import tests_dir
 
+# cleans up test output, this hook is supposed to fail
+sys.stderr = open(os.devnull, 'w')
 
 class Hooks(ApacheMultiSiteApp):
 
