b8511aacb6841b045ff81150ba0a66a1daa55c63
Adrian Moisey <adrian@changeover.za.net>
2015-02-25 12:44:15 +0200

    Allow build to take a directory parameter

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,14 @@ set -efxu
 
 cd "$(dirname "$0")/.."
 
+# Allow overriding default of "build" for the directory name.
+dirname=${1:-build}
+
 # remove dist directory if exists
-[ -d ./build ] && rm -Rf build
-mkdir build
+rm -Rf $dirname
+mkdir $dirname
 
 # copy source to dist
-cp requirements.txt build
-cp -R yodeploy build
-find build -name '*.pyc' -delete
+cp requirements.txt $dirname
+cp -R yodeploy $dirname
+find $dirname -name '*.pyc' -delete
