e9ade45c613774d43181e77e1081410008609da7
Serhii Karelov <sergey.karelov@gmail.com>
2018-05-07 15:13:19 +0300

    Remove legacy tests

--- yodeploy/test_integration/test_django_hooks.py
+++ yodeploy/test_integration/test_django_hooks.py
@@ -15,32 +15,12 @@ class TestDjangoAppDeployment(DeployTestCase):
         self.assertDeployed('deployed/django-app/logs/app-celery.log')
 
 
-class TestApacheHostedDjangoAppDeployment(ApacheTestCase):
-    def setUp(self):
-        self.clear_confs()
-        build_sample('configs')
-        build_sample('apache-hosted-django-app')
-        deploy_sample('apache-hosted-django-app')
-
-    def test_places_a_wsgi_handler(self):
-        self.assertDeployed(
-            'deployed/apache-hosted-django-app/live/'
-            'apache-hosted-django-app.wsgi'
-        )
-
-
 class TestDjangoMultisiteDeployment(ApacheTestCase):
     def setUp(self):
         self.clear_confs()
         build_sample('configs')
         build_sample('apache-hosted-django-multisite')
         deploy_sample('apache-hosted-django-multisite')
-
-    def test_places_a_wsgi_handler(self):
-        self.assertDeployed(
-            'deployed/apache-hosted-django-multisite/'
-            'live/apache-hosted-django-multisite.wsgi'
-        )
 
     def test_places_vhosts(self):
         self.assertDeployed('etc/apache2/sites-enabled/django-siteone.conf')
