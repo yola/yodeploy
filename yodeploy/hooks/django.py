6b3bcaa79196bf35d911eb22224582473a89e620
Serhii Karelov <sergey.karelov@gmail.com>
2018-05-07 15:09:22 +0300

    Remove wsgi-handler.wsgi.template from ApacheHostedDjangoApp

--- yodeploy/hooks/django.py
+++ yodeploy/hooks/django.py
@@ -144,12 +144,7 @@ class DjangoApp(ConfiguratedApp, PythonApp):
 
 
 class ApacheHostedDjangoApp(DjangoApp, ApacheHostedApp):
-    def prepare(self):
-        super(ApacheHostedDjangoApp, self).prepare()
-        if self.template_exists('apache2/wsgi-handler.wsgi.template'):
-            self.template(
-                'apache2/wsgi-handler.wsgi.template',
-                self.deploy_path(self.app + '.wsgi'))
+    pass
 
 
 class ApacheHostedDjangoMultiSiteApp(
