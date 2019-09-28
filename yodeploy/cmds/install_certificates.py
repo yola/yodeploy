42a1a87d5696b7ff44956f08441bb9510eccba88
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-28 14:23:01 -0700

    --clean: prompt for confirmation before cleaning
    There isn't a mechanism to tell whether or not yodeploy owns the assets in the directory targeted by clean. As an added safety measure, this changes --clean to list the contents of those directories and ask the user for confirmation before proceeding.
    
    Additionally, this adds a --no-input flag which skips this confirmation prompt, preserving the ability to use this command in unattended scripts.

--- yodeploy/cmds/install_certificates.py
+++ yodeploy/cmds/install_certificates.py
@@ -13,6 +13,14 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from yodeploy import config  # noqa
 
+# overrwrite the unsafe python 2 input method with raw_input, which does not
+# eval user input. In Python 3, raw_input has been renamed to input. This
+# assignment will raise a NameError, doing nothing.
+try:
+    input = raw_input
+except NameError:
+    pass
+
 
 class CertificateService(HTTPServiceClient):
     def _get_filename_from_headers(self, headers, fallback):
@@ -58,20 +66,54 @@ def find_ssl_path(settings):
     return ssl_dir
 
 
-def remove_all_files(path):
-    for f in os.listdir(path):
-        remove_path = os.path.join(path, f)
-        if os.path.isfile(remove_path):
-            print('removing {}'.format(remove_path))
-            os.unlink(remove_path)
+def get_files(*paths):
+    for path in paths:
+        for f in os.listdir(path):
+            full_path = os.path.join(path, f)
+
+            if os.path.isfile(full_path):
+                yield full_path
 
 
-def clean_ssl_directory(settings):
+def remove_all_files(files):
+    for file in files:
+        print('removing {}'.format(file))
+        os.unlink(file)
+
+
+def clean_ssl_directory(settings, confirm=True):
     ssl_dir = os.path.expanduser(settings.yodeploy.ssl_path)
     certs_dir = os.path.join(ssl_dir, 'certs')
     private_dir = os.path.join(ssl_dir, 'private')
-    remove_all_files(certs_dir)
-    remove_all_files(private_dir)
+    clean_message = '{} and {} are clean.'.format(certs_dir, private_dir)
+
+    # Take care here not to generate this list before and again after the
+    # confirmation prompt. If one were to do so and a file was written to a
+    # target directory before the remove operation executed, we would end up
+    # removing something the user hadn't approved
+    to_remove = list(get_files(certs_dir, private_dir))
+
+    if not to_remove:
+        print(clean_message)
+        return
+
+    if confirm:
+        for f in to_remove:
+            print(f)
+
+    while confirm:
+        response = input('Do you want to delete these files? (y/n) ')
+        response = response.lower()
+
+        if response == 'y':
+            break
+
+        if response == 'n':
+            return
+
+    remove_all_files(to_remove)
+
+    print(clean_message)
 
 
 def main():
@@ -86,6 +128,8 @@ def main():
     parser.add_argument(
         '--clean', action='store_true',
         help='Deletes all ssl certificates in dir managed by this app')
+    parser.add_argument(
+        '--no-input', action='store_true', help='Do not ask for confirmation.')
     options = parser.parse_args()
 
     deploy_config = config.find_deploy_config()
@@ -99,7 +143,7 @@ def main():
             print('{name} -- {developer}'.format(**env))
 
     if options.clean:
-        clean_ssl_directory(settings)
+        clean_ssl_directory(settings, confirm=not options.no_input)
 
     if options.install:
         name = options.install
