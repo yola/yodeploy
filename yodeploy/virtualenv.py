2b5ab64f89717d3fe5a5184268a9f1d15f55d549
fr33l <andrii.zakharkiv@yola.com>
2025-02-14 17:59:06 -0500

    basically fork old virtualenv version

--- yodeploy/virtualenv.py
+++ yodeploy/virtualenv.py
@@ -80,13 +80,13 @@ def create_ve(
             'python%s' % python_version, '-m', 'venv', ve_dir))
         pip_install(ve_dir, pypi, '-U', 'pip')
     elif python_version == sysconfig.get_python_version():
-        virtualenv.create_environment(ve_dir, relocatable=True, site_packages=False)
+        virtualenv.create_environment(ve_dir, site_packages=False)
     else:
         subprocess.check_call((
-            sys.executable, '-m', 'virtualenv', 
+            sys.executable, '-m', 'virtualenv',
             '-p', 'python%s' % python_version,
-            '--system-site-packages', '--relocatable', ve_dir
-            ))
+            '--system-site-packages', ve_dir
+        ))
 
     pip_install(ve_dir, pypi, '-U', 'setuptools')
     pip_install(ve_dir, pypi, 'wheel')
@@ -171,7 +171,7 @@ def relocateable_ve(ve_dir, python_version):
 
     # Python 3 venv virtualenvs don't have these problems
     if python_version == '2.7':
-        virtualenv.create_environment(ve_dir, relocatable=True)
+        virtualenv.make_environment_relocatable(ve_dir)
         fix_local_symlinks(ve_dir)
         remove_fragile_symlinks(ve_dir)
 
@@ -250,7 +250,7 @@ def remove_fragile_symlinks(ve_dir):
     if getattr(sys, 'real_prefix', sys.prefix) == sys.prefix:
         return
     ve_prefix = sys.prefix
-    lib_dir = os.path.join(ve_dir, 'lib')
+    home_dir, lib_dir, inc_dir, bin_dir = virtualenv.path_locations(ve_dir)
     for fn in os.listdir(lib_dir):
         path = os.path.join(lib_dir, fn)
         if not os.path.islink(path):
@@ -262,3 +262,219 @@ def remove_fragile_symlinks(ve_dir):
 
         log.debug('Removing fragile symlink %s -> %s', path, dest)
         os.unlink(path)
+
+
+VERSION = "{}.{}".format(*sys.version_info)
+PY_VERSION = "python{}.{}".format(*sys.version_info)
+ABI_FLAGS = getattr(sys, "abiflags", "")
+OK_ABS_SCRIPTS = [
+    "python",
+    PY_VERSION,
+    "activate",
+    "activate.bat",
+    "activate_this.py",
+    "activate.fish",
+    "activate.csh",
+    "activate.xsh",
+]
+
+
+def make_environment_relocatable(home_dir):
+    """
+    Makes the already-existing environment use relative paths, and takes out
+    the #!-based environment selection in scripts.
+    """
+    home_dir, lib_dir, inc_dir, bin_dir = path_locations(home_dir)
+    activate_this = os.path.join(bin_dir, "activate_this.py")
+    if not os.path.exists(activate_this):
+        log.fatal(
+            "The environment doesn't have a file %s -- please re-run virtualenv " "on this environment to update it",
+            activate_this,
+        )
+    fixup_scripts(home_dir, bin_dir)
+    fixup_pth_and_egg_link(home_dir)
+
+
+def fixup_pth_and_egg_link(home_dir, sys_path=None):
+    """Makes .pth and .egg-link files use relative paths"""
+    home_dir = os.path.normcase(os.path.abspath(home_dir))
+    if sys_path is None:
+        sys_path = sys.path
+    for a_path in sys_path:
+        if not a_path:
+            a_path = "."
+        if not os.path.isdir(a_path):
+            continue
+        a_path = os.path.normcase(os.path.abspath(a_path))
+        if not a_path.startswith(home_dir):
+            log.debug("Skipping system (non-environment) directory %s", a_path)
+            continue
+        for filename in os.listdir(a_path):
+            filename = os.path.join(a_path, filename)
+            if filename.endswith(".pth"):
+                if not os.access(filename, os.W_OK):
+                    log.warn("Cannot write .pth file %s, skipping", filename)
+                else:
+                    fixup_pth_file(filename)
+            if filename.endswith(".egg-link"):
+                if not os.access(filename, os.W_OK):
+                    log.warn("Cannot write .egg-link file %s, skipping", filename)
+                else:
+                    fixup_egg_link(filename)
+
+
+def fixup_scripts(_, bin_dir):
+    new_shebang_args = ("/usr/bin/env", VERSION, "")
+
+    # This is what we expect at the top of scripts:
+    shebang = "#!{}".format(
+        os.path.normcase(os.path.join(os.path.abspath(bin_dir), "python{}".format(new_shebang_args[2])))
+    )
+    # This is what we'll put:
+    new_shebang = "#!{} python{}{}".format(*new_shebang_args)
+
+    for filename in os.listdir(bin_dir):
+        filename = os.path.join(bin_dir, filename)
+        if not os.path.isfile(filename):
+            # ignore child directories, e.g. .svn ones.
+            continue
+        with open(filename, "rb") as f:
+            try:
+                lines = f.read().decode("utf-8").splitlines()
+            except UnicodeDecodeError:
+                # This is probably a binary program instead
+                # of a script, so just ignore it.
+                continue
+        if not lines:
+            log.warn("Script %s is an empty file", filename)
+            continue
+
+        old_shebang = lines[0].strip()
+        old_shebang = old_shebang[0:2] + os.path.normcase(old_shebang[2:])
+
+        if not old_shebang.startswith(shebang):
+            if os.path.basename(filename) in OK_ABS_SCRIPTS:
+                log.debug("Cannot make script %s relative", filename)
+            elif lines[0].strip() == new_shebang:
+                log.info("Script %s has already been made relative", filename)
+            else:
+                log.warn(
+                    "Script %s cannot be made relative (it's not a normal script that starts with %s)",
+                    filename,
+                    shebang,
+                )
+            continue
+        log.notify("Making script %s relative", filename)
+        script = relative_script([new_shebang] + lines[1:])
+        with open(filename, "wb") as f:
+            f.write("\n".join(script).encode("utf-8"))
+
+
+def fixup_egg_link(filename):
+    with open(filename) as f:
+        link = f.readline().strip()
+    if os.path.abspath(link) != link:
+        log.debug("Link in %s already relative", filename)
+        return
+    new_link = make_relative_path(filename, link)
+    log.notify("Rewriting link {} in {} as {}".format(link, filename, new_link))
+    with open(filename, "w") as f:
+        f.write(new_link)
+
+
+def make_relative_path(source, dest, dest_is_directory=True):
+    """
+    Make a filename relative, where the filename is dest, and it is
+    being referred to from the filename source.
+
+        >>> make_relative_path('/usr/share/something/a-file.pth',
+        ...                    '/usr/share/another-place/src/Directory')
+        '../another-place/src/Directory'
+        >>> make_relative_path('/usr/share/something/a-file.pth',
+        ...                    '/home/user/src/Directory')
+        '../../../home/user/src/Directory'
+        >>> make_relative_path('/usr/share/a-file.pth', '/usr/share/')
+        './'
+    """
+    source = os.path.dirname(source)
+    if not dest_is_directory:
+        dest_filename = os.path.basename(dest)
+        dest = os.path.dirname(dest)
+    else:
+        dest_filename = None
+    dest = os.path.normpath(os.path.abspath(dest))
+    source = os.path.normpath(os.path.abspath(source))
+    dest_parts = dest.strip(os.path.sep).split(os.path.sep)
+    source_parts = source.strip(os.path.sep).split(os.path.sep)
+    while dest_parts and source_parts and dest_parts[0] == source_parts[0]:
+        dest_parts.pop(0)
+        source_parts.pop(0)
+    full_parts = [".."] * len(source_parts) + dest_parts
+    if not dest_is_directory and dest_filename is not None:
+        full_parts.append(dest_filename)
+    if not full_parts:
+        # Special case for the current directory (otherwise it'd be '')
+        return "./"
+    return os.path.sep.join(full_parts)
+
+
+def fixup_pth_file(filename):
+    lines = []
+    with open(filename) as f:
+        prev_lines = f.readlines()
+    for line in prev_lines:
+        line = line.strip()
+        if not line or line.startswith("#") or line.startswith("import ") or os.path.abspath(line) != line:
+            lines.append(line)
+        else:
+            new_value = make_relative_path(filename, line)
+            if line != new_value:
+                log.debug("Rewriting path {} as {} (in {})".format(line, new_value, filename))
+            lines.append(new_value)
+    if lines == prev_lines:
+        log.info("No changes to .pth file %s", filename)
+        return
+    log.notify("Making paths in .pth file %s relative", filename)
+    with open(filename, "w") as f:
+        f.write("\n".join(lines) + "\n")
+
+
+FILE_PATH = __file__ if os.path.isabs(__file__) else os.path.join(os.getcwd(), __file__)
+
+
+def relative_script(lines):
+    """Return a script that'll work in a relocatable environment."""
+    activate = (
+        "import os; "
+        "activate_this=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'activate_this.py'); "
+        "exec(compile(open(activate_this).read(), activate_this, 'exec'), { '__file__': activate_this}); "
+        "del os, activate_this"
+    )
+    # Find the last future statement in the script. If we insert the activation
+    # line before a future statement, Python will raise a SyntaxError.
+    activate_at = None
+    for idx, line in reversed(list(enumerate(lines))):
+        if line.split()[:3] == ["from", "__future__", "import"]:
+            activate_at = idx + 1
+            break
+    if activate_at is None:
+        # Activate after the shebang.
+        activate_at = 1
+    return lines[:activate_at] + ["", activate, ""] + lines[activate_at:]
+
+
+def path_locations(home_dir, dry_run=False):
+    """Return the path locations for the environment (where libraries are,
+    where scripts go, etc)"""
+    home_dir = os.path.abspath(home_dir)
+    lib_dir, inc_dir, bin_dir = None, None, None
+    lib_dir = join(home_dir, "lib", PY_VERSION)
+    inc_dir = join(home_dir, "include", PY_VERSION + ABI_FLAGS)
+    bin_dir = join(home_dir, "bin")
+    return home_dir, lib_dir, inc_dir, bin_dir
+
+
+def join(*chunks):
+    line = "".join(chunks)
+    sep = ("\\" in line and "\\") or ("/" in line and "/") or "/"
+    return sep.join(chunks)
