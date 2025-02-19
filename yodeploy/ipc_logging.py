4bf4483dbf8d5328be567d09c5f0b429acf9ea12
fr33l <andrii.zakharkiv@yola.com>
2025-02-19 12:36:56 -0500

    rewrite this

--- yodeploy/ipc_logging.py
+++ yodeploy/ipc_logging.py
@@ -1,29 +1,10 @@
-"""
-Provides a ThreadedLogStreamServer which receives and logs messages via socket.
-
-Python subprocesses may make use of the provided ExistingSocketHandler loggging
-handler to route log statments to a running ThreadedLogStreamServer provided
-that they have a file descriptor for the socket that the TLSS instance is
-listening on. Typically this is achieved by passing subprocesses an appropriate
-file descriptor as an argument.
-
-Note about Python > 3.4: File descriptors are no longer inheritable by default.
-TLSS instances explicitly mark their file descriptors for the sockets they open
-as inheritable so that subprocess may access them.
-
-"""
-import errno
-import logging
-import logging.handlers
-import pickle
 import socket
 import struct
-
-try:
-    import socketserver  # python 3
-except:
-    import socketserver as socketserver  # python 2
-
+import pickle
+import logging
+import logging.handlers
+import errno
+import socketserver
 
 class ExistingSocketHandler(logging.handlers.SocketHandler):
     """Logging handler that writes messages to a pre-created socket."""
@@ -32,15 +13,9 @@ class ExistingSocketHandler(logging.handlers.SocketHandler):
         logging.handlers.SocketHandler.__init__(self, None, None)
         self.sock = sock
 
-
 class LoggingSocketRequestHandler(socketserver.BaseRequestHandler):
-    """SocketServer handler that un-pickles SocketHandler log messages."""
-
-    def __init__(self, request, client_address, server, oneshot=False):
-        self._oneshot = oneshot
-        socketserver.BaseRequestHandler.__init__(self, request, client_address,
-                                                 server)
-
+    _oneshot = False
+    
     def handle(self):
         buf = b''
         header_size = struct.calcsize('>L')
@@ -49,39 +24,35 @@ class LoggingSocketRequestHandler(socketserver.BaseRequestHandler):
                 if len(buf) < header_size:
                     buf += self.request.recv(header_size - len(buf))
                     continue
+
                 size = struct.unpack('>L', buf[:header_size])[0] + header_size
                 if len(buf) < size:
                     buf += self.request.recv(size - len(buf))
                     continue
+
             except IOError as e:
                 if e.errno == errno.EBADF:
                     break
-            record = pickle.loads(buf[header_size:])
+                raise
+
+            record = pickle.loads(buf[header_size:size])
             record = logging.makeLogRecord(record)
             logger = logging.getLogger(record.name)
             if logger.isEnabledFor(record.levelno):
                 logger.handle(record)
+            
             buf = buf[size:]
-
             if self._oneshot:
                 break
 
-
-class ThreadedLogStreamServer(socketserver.ThreadingMixIn,
-                              socketserver.UnixStreamServer):
+class ThreadedLogStreamServer(socketserver.ThreadingMixIn, socketserver.UnixStreamServer):
     """Server that will listen for log messages sent via a socket."""
-
+    
     def __init__(self):
         self.socket, self.remote_socket = socket.socketpair(
             socket.AF_UNIX, socket.SOCK_STREAM)
-
-        try:
-            self.remote_socket.set_inheritable(True)
-        except AttributeError:
-            # file descriptors are all inheritable in python < 3.4, so this
-            # method does not exist in prior versions.
-            pass
-
+        
+        self.remote_socket.set_inheritable(True)
         self.RequestHandlerClass = LoggingSocketRequestHandler
         self.process_request(self.socket, None)
 
--- yodeploy/tests/test_ipc_logging.py
+++ yodeploy/tests/test_ipc_logging.py
@@ -5,17 +5,16 @@ import socket
 import struct
 import subprocess
 import sys
-import unittest
-
 from io import StringIO
 
 from yodeploy.ipc_logging import (
-    ExistingSocketHandler, LoggingSocketRequestHandler,
-    ThreadedLogStreamServer)
+    LoggingSocketRequestHandler,
+    ThreadedLogStreamServer,
+    ExistingSocketHandler
+)
 from yodeploy.tests import HelperScriptConsumer, yodeploy_location
 
-
-class TestExistingSocketHandler(unittest.TestCase):
+class TestExistingSocketHandler:
     def test_handle(self):
         a, b = socket.socketpair(socket.AF_UNIX, socket.SOCK_STREAM)
         a.settimeout(2)
@@ -24,108 +23,151 @@ class TestExistingSocketHandler(unittest.TestCase):
         try:
             sh = ExistingSocketHandler(a)
             r = logging.LogRecord('test', logging.INFO, __file__, 42,
-                                  'Testing 123', [], None, 'test_handle')
+                                'Testing 123', [], None, 'test_handle')
             sh.handle(r)
             sh.close()
             size = struct.unpack('>L', b.recv(4))[0]
             received = pickle.loads(b.recv(size))
-            self.assertIsInstance(received, dict)
-            self.assertEqual(received['msg'], 'Testing 123')
+            assert isinstance(received, dict)
+            assert received['msg'] == 'Testing 123'
         finally:
             a.close()
             b.close()
 
-
-class TestLoggingSocketRequestHandler(unittest.TestCase):
-    def test_handle(self):
-        logger = logging.getLogger('test')
-        buffer_ = StringIO()
-        handler = logging.StreamHandler(buffer_)
-        logger.addHandler(handler)
-
-        a, b = socket.socketpair(socket.AF_UNIX, socket.SOCK_STREAM)
-        a.settimeout(2)
-        b.settimeout(2)
+class TestLoggingSocketRequestHandler:
+    def setup_method(self):
+        self.logger = logging.getLogger('test')
+        self.buffer = StringIO()
+        self.handler = logging.StreamHandler(self.buffer)
+        self.logger.addHandler(self.handler)
         
-        try:
-            r = logging.LogRecord('test', logging.INFO, __file__, 42,
-                                  'Testing 123', [], None, 'test_handle')
-            data = pickle.dumps(r.__dict__)
-            a.send(struct.pack('>L', len(data)))
-            a.send(data)
+        self.socket_pair = socket.socketpair(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.socket_a, self.socket_b = self.socket_pair
+        self.socket_a.settimeout(2)
+        self.socket_b.settimeout(2)
 
-            LoggingSocketRequestHandler(b, None, None, oneshot=True)
-            size = struct.unpack('>L', b.recv(4))[0]
-            received = pickle.loads(b.recv(size))
-            self.assertIsInstance(received, dict)
-            self.assertEqual(received['msg'], 'Testing 123')
+    def teardown_method(self):
+        self.handler.flush()
+        self.logger.removeHandler(self.handler)
+        self.socket_a.close()
+        self.socket_b.close()
 
-            b.close()
-        finally:
-            a.close()
+    def test_handle(self):
+        """Test basic message handling."""
+        r = logging.LogRecord('test', logging.INFO, __file__, 42,
+                            'Testing 123', [], None, 'test_handle')
+        data = pickle.dumps(r.__dict__)
+        self.socket_a.send(struct.pack('>L', len(data)))
+        self.socket_a.send(data)
+
+        LoggingSocketRequestHandler(self.socket_b, None, None, oneshot=True)
+        size = struct.unpack('>L', self.socket_b.recv(4))[0]
+        received = pickle.loads(self.socket_b.recv(size))
+        assert isinstance(received, dict)
+        assert received['msg'] == 'Testing 123'
 
     def test_filtered(self):
-        logger = logging.getLogger('test')
-        logger.setLevel(logging.WARN)
-        buffer_ = StringIO()
-        handler = logging.StreamHandler(buffer_)
-        logger.addHandler(handler)
+        """Test message filtering based on log level."""
+        self.logger.setLevel(logging.WARN)
+        
+        r = logging.LogRecord('test', logging.INFO, __file__, 42,
+                            'Testing 123', [], None, 'test_handle')
+        data = pickle.dumps(r.__dict__)
+        self.socket_a.send(struct.pack('>L', len(data)))
+        self.socket_a.send(data)
 
-        a, b = socket.socketpair(socket.AF_UNIX, socket.SOCK_STREAM)
-        a.settimeout(2)
-        b.settimeout(2)
+        LoggingSocketRequestHandler(self.socket_b, None, None, oneshot=True)
+        self.handler.flush()
+        self.logger.removeHandler(self.handler)
+        self.logger.setLevel(logging.NOTSET)
+        assert not self.buffer.getvalue()
+
+    def test_handle_multiple_records(self):
+        """Test handling of multiple records."""
+        messages = ["Message 1", "Message 2", "Message 3"]
+        
+        for msg in messages:
+            r = logging.LogRecord('test', logging.INFO, __file__, 42,
+                                msg, [], None, 'test_handle')
+            data = pickle.dumps(r.__dict__)
+            self.socket_a.send(struct.pack('>L', len(data)))
+            self.socket_a.send(data)
+
+        handler = LoggingSocketRequestHandler(
+            self.socket_b, None, None, oneshot=False)
+        handler_thread = threading.Thread(target=handler.handle)
+        handler_thread.start()
+        
+        self.socket_a.close()
+        handler_thread.join()
+        
+        output = self.buffer.getvalue().strip().split('\n')
+        assert len(output) == len(messages)
+
+class TestThreadedLogStreamServer(HelperScriptConsumer):
+    def setup_method(self):
+        self.server = ThreadedLogStreamServer()
+        self.logger = logging.getLogger('test')
+        self.logger.propagate = False
+        self.buffer = StringIO()
+        self.handler = logging.StreamHandler(self.buffer)
+        self.logger.addHandler(self.handler)
+
+    def teardown_method(self):
+        if self.server:
+            self.server.shutdown()
+        self.handler.flush()
+        self.logger.removeHandler(self.handler)
+        self.logger.propagate = True
+
+    def test_server_initialization(self):
+        """Test server initialization and socket creation."""
+        assert self.server.socket is not None
+        assert self.server.remote_socket is not None
+        assert self.server.remote_socket.get_inheritable()
+
+    def test_server_shutdown(self):
+        """Test server shutdown and socket cleanup."""
+        socket_fd = self.server.socket.fileno()
+        remote_fd = self.server.remote_socket.fileno()
+        
+        self.server.shutdown()
         
         try:
-            r = logging.LogRecord('test', logging.INFO, __file__, 42,
-                                  'Testing 123', [], None, 'test_handle')
-            data = pickle.dumps(r.__dict__)
-            a.send(struct.pack('>L', len(data)))
-            a.send(data)
+            os.fstat(socket_fd)
+            assert False, "Socket should be closed"
+        except OSError:
+            pass
 
-            LoggingSocketRequestHandler(b, None, None, oneshot=True)
-            handler.flush()
-            logger.removeHandler(handler)
-            logger.setLevel(logging.NOTSET)
-            self.assertFalse(buffer_.getvalue())
-            b.close()
-        finally:
-            a.close()
-
-
-class TestThreadedLogStreamServer(unittest.TestCase, HelperScriptConsumer):
-    def setUp(self):
-        super().setUp()
-        self.tlss = ThreadedLogStreamServer()
-        self.addCleanup(self.tlss.shutdown)
+        try:
+            os.fstat(remote_fd)
+            assert False, "Remote socket should be closed"
+        except OSError:
+            pass
 
     def test_integration(self):
-        logger = logging.getLogger('test')
-        logger.propagate = False  # Fixed typo
-        buffer_ = StringIO()
-        handler = logging.StreamHandler(buffer_)
-        logger.addHandler(handler)
-
+        """Test integration with subprocess."""
         helper_script = self.get_helper_path('tlss_user.py')
         assert os.path.exists(helper_script), f"Missing helper script: {helper_script}"
 
         try:
             p = subprocess.Popen(
-                [sys.executable, helper_script, str(self.tlss.remote_socket.fileno())],
-                env={'PATH': os.environ['PATH'], 'PYTHONPATH': yodeploy_location()},
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+                [sys.executable, helper_script, str(self.server.remote_socket.fileno())],
+                env={
+                    'PATH': os.environ['PATH'],
+                    'PYTHONPATH': yodeploy_location()
+                },
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True
             )
 
-            out, err = p.communicate(timeout=5)  # Prevent indefinite hanging
+            out, err = p.communicate(timeout=5)
             exit_code = p.wait()
 
-            self.assertEqual(exit_code, 0, f'Subprocess outputted: {out}{err}')
-            self.assertTrue(buffer_.getvalue().strip())
+            assert exit_code == 0, f'Subprocess outputted: {out}{err}'
+            assert self.buffer.getvalue().strip()
 
         except subprocess.TimeoutExpired:
             p.terminate()
-            self.fail("Subprocess timeout expired")
-        
-        finally:
-            handler.flush()
-            logger.removeHandler(handler)
-            logger.propagate = True
+            raise AssertionError("Subprocess timeout expired")
